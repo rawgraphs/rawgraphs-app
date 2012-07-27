1000a12e41865baed20d73e5aedddfecbf53bf46
Andrew Hobden <andy.hobden@gmail.com>
2012-07-27 09:39:55 -0700

    Fixed path to read api.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,7 +3,7 @@
 /* Controllers */
 
 function AppCtrl($scope, $http) {
-  $http({method: 'GET', url: '/json/name'}).
+  $http({method: 'GET', url: '/api/name'}).
   success(function(data, status, headers, config) {
     $scope.name = data.name;
   }).
