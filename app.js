5a1134e70230f0d9d05239bd686d5f3a261db32c
thewildpendulum <tim@flywheelcollective.com>
2012-10-13 10:27:09 -0500

    updated express to version 3.0.0

--- app.js
+++ app.js
@@ -7,16 +7,13 @@ var express = require('express'),
   routes = require('./routes'),
   api = require('./routes/api');
 
-var app = module.exports = express.createServer();
+var app = module.exports = express();
 
 // Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.set('view options', {
-    layout: false
-  });
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.static(__dirname + '/public'));
@@ -46,5 +43,5 @@ app.get('*', routes.index);
 // Start server
 
 app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
