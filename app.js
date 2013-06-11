4d63cf8ac78024c61d9bd39839c70df79e07d174
Brian Ford <btford@umich.edu>
2013-06-11 13:56:27 -0700

    updated express and jade dependencies

--- app.js
+++ app.js
@@ -1,47 +1,61 @@
 
 /**
- * Module dependencies.
+ * Module dependencies
  */
 
 var express = require('express'),
   routes = require('./routes'),
-  api = require('./routes/api');
+  api = require('./routes/api'),
+  http = require('http'),
+  path = require('path');
 
 var app = module.exports = express();
 
-// Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/public'));
-  app.use(app.router);
-});
+/**
+ * Configuration
+ */
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
 
-app.configure('production', function(){
+// development only
+if (app.get('env') === 'development') {
   app.use(express.errorHandler());
-});
+}
 
-// Routes
+// production only
+if (app.get('env') === 'production') {
+  // TODO
+};
 
+
+/**
+ * Routes
+ */
+
+// serve index and view partials
 app.get('/', routes.index);
 app.get('/partials/:name', routes.partials);
 
 // JSON API
-
 app.get('/api/name', api.name);
 
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
 
-// Start server
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+/**
+ * Start Server
+ */
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
 });
