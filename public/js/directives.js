54e1bb631773aceaec54895823320ba6044de5e6
giorgiocaviglia <giorgio.caviglia@gmail.com>
2013-10-02 01:42:42 +0200

    adding missing .png

--- public/js/directives.js
+++ public/js/directives.js
@@ -439,7 +439,6 @@ angular.module('raw.directives', [])
               .node().parentNode.innerHTML;
 
           canvg('canvas',html);
-         
           var canvas = document.getElementById("canvas");//, ctx = canvas.getContext("2d");
           canvas.toBlob(function(blob) {
               saveAs(blob, (element.find('input').val() || element.find('input').attr("placeholder")) + ".png");
