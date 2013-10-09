62ce411b39d9fbfb6db74f616ce92e242b0116f3
giorgiocaviglia <giorgio.caviglia@gmail.com>
2013-10-09 14:43:33 +0200

    Fixed typos

--- charts/circulardendrogram.js
+++ charts/circulardendrogram.js
@@ -1,10 +1,10 @@
-raw.charts.circulardendogram = function(){
+raw.charts.circulardendrogram = function(){
 	
 	return {
 
-		title : 'Circular Dendogram',
+		title : 'Circular Dendrogram',
 		description : 'Radial tree diagrams imply the same hierarchical logic of the dendrogram, implementing in addition an algorithm to optimize the node distribution along a circular shape, allowing to minimize the needed space. The arrangement of the clusters is calculated by the distance from the central root node. <br>Based on <a href="http://bl.ocks.org/mbostock/4063550">http://bl.ocks.org/mbostock/4063550</a>.',
-		image : 'charts/imgs/circular-dendogram.png',
+		image : 'charts/imgs/circular-dendrogram.png',
 		model : raw.models.hierarchy(),
 
 		options : {
