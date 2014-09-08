e73782818ffd137220d4422bea301440f4eaf51b
giorgiocaviglia <giorgio.caviglia@gmail.com>
2014-09-08 15:06:23 -0700

    Inverting Packing order, closes #70

--- charts/packing.js
+++ charts/packing.js
@@ -46,7 +46,7 @@
 
 		var pack = d3.layout.pack()
 		    .padding(+padding())
-		    .sort(function (a,b){ return sort() ? a.value-b.value : null; })
+		    .sort(function (a,b){ return sort() ? b.value - a.value : null; })
 		    .size([innerDiameter, innerDiameter])
 		    .value(function(d) { return +d.size; })
 
