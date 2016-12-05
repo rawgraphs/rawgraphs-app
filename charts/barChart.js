87f0df790c3bfcd3c124a91bfe4bce9c9a8cc6c9
mikima <mikimauri@gmail.com>
2016-12-05 12:02:25 +0100

    removed auto-sorting in barcharts
    now will be used the original order in the data

--- charts/barChart.js
+++ charts/barChart.js
@@ -40,6 +40,7 @@
 		var results = d3.nest()
 						.key(groups)
 						.key(categories)
+						.sortKeys(function(d){ return 1})
 						.rollup(function(g){
 							//get all the variables from the first item
 							var result = g.map(function(d){
