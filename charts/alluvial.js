f38e394ef8000228ae6c3b3056634de2376b469c
vathsav <vathsav@live.in>
2017-06-23 09:06:54 +0700

    feat: updated to d3v4

--- charts/alluvial.js
+++ charts/alluvial.js
@@ -1,160 +1,196 @@
-(function(){
+(function() {
 
 	var graph = raw.models.graph();
 
 	var chart = raw.chart()
 		.title('Alluvial Diagram')
 		.description(
-            "Alluvial diagrams allow to represent flows and to see correlations between categorical dimensions, visually linking to the number of elements sharing the same categories. It is useful to see the evolution of cluster (such as the number of people belonging to a specific group). It can also be used to represent bipartite graphs, using each node group as dimensions.<br/>Mainly based on DensityDesign's work with Fineo, it is inspired by <a href='http://bost.ocks.org/mike/sankey/'>http://bost.ocks.org/mike/sankey/</a>")
+			"Alluvial diagrams allow to represent flows and to see correlations between categorical dimensions, visually linking to the number of elements sharing the same categories. It is useful to see the evolution of cluster (such as the number of people belonging to a specific group). It can also be used to represent bipartite graphs, using each node group as dimensions.<br/>Mainly based on DensityDesign's work with Fineo, it is inspired by <a href='http://bost.ocks.org/mike/sankey/'>http://bost.ocks.org/mike/sankey/</a>")
 		.thumbnail("imgs/alluvial.png")
 		.category("Multi categorical")
-		.model(graph)
+		.model(graph);
 
 	var width = chart.number()
 		.title("Width")
 		.defaultValue(1000)
-		.fitToWidth(true)
+		.fitToWidth(true);
 
 	var height = chart.number()
 		.title("Height")
-		.defaultValue(500)
+		.defaultValue(500);
 
 	var nodeWidth = chart.number()
 		.title("Node Width")
-		.defaultValue(5)
+		.defaultValue(5);
 
 	var sortBy = chart.list()
-        .title("Sort by")
-        .values(['size','name','automatic'])
-        .defaultValue('size')
+		.title("Sort by")
+		.values(['size', 'name', 'automatic'])
+		.defaultValue('size');
 
 	var colors = chart.color()
-		.title("Color scale")
+		.title("Color scale");
 
-	chart.draw(function (selection, data){
+	chart.draw((selection, data) => {
 
 		var formatNumber = d3.format(",.0f"),
-		    format = function(d) { return formatNumber(d); };
+			format = d => {
+				return formatNumber(d);
+			};
 
 		var g = selection
-		    .attr("width", +width() )
-		    .attr("height", +height() + 20 )
-		  	.append("g")
-		    .attr("transform", "translate(" + 0 + "," + 10 + ")");
+			.attr("width", +width())
+			.attr("height", +height() + 20)
+			.append("g")
+			.attr("transform", "translate(0, 10)");
 
 		// Calculating the best nodePadding
 
 		var nested = d3.nest()
-	    	.key(function (d){ return d.group; })
-	    	.rollup(function (d){ return d.length; })
-	    	.entries(data.nodes)
+			.key(d => {
+				return d.group;
+			})
+			.rollup(d => {
+				return d.length;
+			})
+			.entries(data.nodes);
 
-	    var maxNodes = d3.max(nested, function (d){ return d.values; });
+		var maxNodes = d3.max(nested, d => {
+			return d.values;
+		});
 
 		var sankey = d3.sankey()
-		    .nodeWidth(+nodeWidth())
-		    .nodePadding(d3.min([10,(height()-maxNodes)/maxNodes]))
-		    .size([+width(), +height()]);
+			.nodeWidth(+nodeWidth())
+			.nodePadding(d3.min([10, (height() - maxNodes) / maxNodes]))
+			.size([+width(), +height()]);
 
 		var path = sankey.link(),
 			nodes = data.nodes,
 			links = data.links;
 
 		sankey
-	   		.nodes(nodes)
-	    	.links(links)
-	    	.layout(32);
+			.nodes(nodes)
+			.links(links)
+			.layout(32);
 
-	    // Re-sorting nodes
+		// Re-sorting nodes
 
-	    nested = d3.nest()
-	    	.key(function(d){ return d.group; })
-	    	.map(nodes)
+		nested = d3.nest()
+			.key(d => {
+				return d.group;
+			})
+			.map(nodes);
 
-	    d3.values(nested)
-	    	.forEach(function (d){
-		    	var y = ( height() - d3.sum(d,function(n){ return n.dy+sankey.nodePadding();}) ) / 2 + sankey.nodePadding()/2;
-		    	d.sort(function (a,b){
-		    		if (sortBy() == "automatic") return b.y - a.y;
-		    		if (sortBy() == "size") return b.dy - a.dy;
-		    		if (sortBy() == "name") return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
-		    	})
-		    	d.forEach(function (node){
-		    		node.y = y;
-		    		y += node.dy +sankey.nodePadding();
-		    	})
-		    })
+		Object.values(nested)
+			.forEach(d => {
+				var y = (height() - d3.sum(d, n => {
+					return n.dy + sankey.nodePadding();
+				})) / 2 + sankey.nodePadding() / 2;
+				d.sort((a, b) => {
+					if(sortBy() == "automatic") return b.y - a.y;
+					if(sortBy() == "size") return b.dy - a.dy;
+					if(sortBy() == "name") return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+				})
+				d.forEach(node => {
+					node.y = y;
+					y += node.dy + sankey.nodePadding();
+				})
+			});
 
-	    // Resorting links
+		// Resorting links
 
-		d3.values(nested).forEach(function (d){
+		Object.values(nested).forEach(d => {
 
-			d.forEach(function (node){
+			d.forEach(node => {
 
-	    		var ly = 0;
-	    		node.sourceLinks
-		    		.sort(function (a,b){
-		    			return a.target.y - b.target.y;
-		    		})
-		    		.forEach(function (link){
-		    			link.sy = ly;
-		    			ly += link.dy;
-		    		})
+				var ly = 0;
+				node.sourceLinks
+					.sort((a, b) => {
+						return a.target.y - b.target.y;
+					})
+					.forEach(link => {
+						link.sy = ly;
+						ly += link.dy;
+					});
 
-		    	ly = 0;
+				ly = 0;
 
-		    	node.targetLinks
-		    		.sort(function(a,b){
-		    			return a.source.y - b.source.y;
-		    		})
-		    		.forEach(function (link){
-		    			link.ty = ly;
-		    			ly += link.dy;
-		    		})
+				node.targetLinks
+					.sort((a, b) => {
+						return a.source.y - b.source.y;
+					})
+					.forEach(link => {
+						link.ty = ly;
+						ly += link.dy;
+					});
 			})
 		})
 
-	 	colors.domain(links, function (d){ return d.source.name; });
+		colors.domain(links, d => {
+			return d.source.name;
+		});
 
 		var link = g.append("g").selectAll(".link")
-	    	.data(links)
-	   		.enter().append("path")
-			    .attr("class", "link")
-			    .attr("d", path )
-			    .style("stroke-width", function(d) { return Math.max(1, d.dy); })
-			    .style("fill","none")
-			    .style("stroke", function (d){ return colors()(d.source.name); })
-			    .style("stroke-opacity",".4")
-			    .sort(function(a, b) { return b.dy - a.dy; })
-			    .append("title")
-			    .text(function(d) { console.log(d); return d.value});
+			.data(links)
+			.enter().append("path")
+			.attr("class", "link")
+			.attr("d", path)
+			.style("stroke-width", d => {
+				return Math.max(1, d.dy);
+			})
+			.style("fill", "none")
+			.style("stroke", d => {
+				return colors()(d.source.name);
+			})
+			.style("stroke-opacity", ".4")
+			.sort((a, b) => {
+				return b.dy - a.dy;
+			})
+			.append("title")
+			.text(d => {
+				return d.value;
+			});
 
 		var node = g.append("g").selectAll(".node")
-	    	.data(nodes)
-	    	.enter().append("g")
-		      	.attr("class", "node")
-		      	.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+			.data(nodes)
+			.enter().append("g")
+			.attr("class", "node")
+			.attr("transform", d => {
+				return `translate(${d.x}, ${d.y})`;
+			});
 
 		node.append("rect")
-		    .attr("height", function(d) { return d.dy; })
-		    .attr("width", sankey.nodeWidth())
-		    .style("fill", function (d) { return d.sourceLinks.length ? colors(d.name) : "#666"; })
-		    .append("title")
-		    	.text(function(d) { return d.name + "\n" + format(d.value); });
+			.attr("height", d => {
+				return d.dy;
+			})
+			.attr("width", sankey.nodeWidth())
+			.style("fill", d => {
+				return d.sourceLinks.length ? colors(d.name) : "#666";
+			})
+			.append("title")
+			.text(d => {
+				return d.name + "\n" + format(d.value);
+			});
 
 		node.append("text")
-		    .attr("x", -6)
-	      	.attr("y", function (d) { return d.dy / 2; })
-	      	.attr("dy", ".35em")
-	      	.attr("text-anchor", "end")
-	      	.attr("transform", null)
-			    .text(function(d) { return d.name; })
-			    .style("font-size","11px")
-				.style("font-family","Arial, Helvetica")
-			    .style("pointer-events","none")
-			    .filter(function(d) { return d.x < +width() / 2; })
-			    .attr("x", 6 + sankey.nodeWidth())
-		     	.attr("text-anchor", "start");
+			.attr("x", -6)
+			.attr("y", d => {
+				return d.dy / 2;
+			})
+			.attr("dy", ".35em")
+			.attr("text-anchor", "end")
+			.attr("transform", null)
+			.text(d => {
+				return d.name;
+			})
+			.style("font-size", "11px")
+			.style("font-family", "Arial, Helvetica")
+			.style("pointer-events", "none")
+			.filter(d => {
+				return d.x < +width() / 2;
+			})
+			.attr("x", 6 + sankey.nodeWidth())
+			.attr("text-anchor", "start");
 
 	})
 
--- charts/convexHullMultiple.js
+++ charts/convexHullMultiple.js
@@ -1,224 +1,219 @@
-(function () {
+(function() {
 
-    var model = raw.model();
+	var model = raw.model();
 
-    var mx = model.dimension()
-        .title("X Axis")
-        .types(Number, Date)
-        .accessor(function (d) {
-            return this.type() == "Date" ? new Date(d) : +d;
-        })
-        .required(1);
+	var mx = model.dimension()
+    .title("X Axis")
+		.types(Number, Date)
+		.accessor(function(d) {
+			return this.type() == "Date" ? new Date(d) : +d;
+		})
+		.required(1);
 
-    var my = model.dimension()
-        .title("Y Axis")
-        .types(Number, Date)
-        .accessor(function (d) {
-            return this.type() == "Date" ? new Date(d) : +d;
-        })
-        .required(1);
+	var my = model.dimension()
+		.title("Y Axis")
+		.types(Number, Date)
+		.accessor(function(d) {
+			return this.type() == "Date" ? new Date(d) : +d;
+		})
+		.required(1);
 
-    var mlabel = model.dimension()
-        .title('label')
+	var mlabel = model.dimension()
+		.title('label')
 
-    var mgroup = model.dimension().title('Group')
+	var mgroup = model.dimension().title('Group')
 
-    model.map(function (data) {
+	model.map(data => {
 
-        var nest = d3.nest()
-            .key(mgroup)
-            .rollup(function (g) {
-                return g.map(function (d) {
-                    return {
-                        group: mgroup(d),
-                        y: my(d),
-                        x: mx(d),
-                        label: mlabel(d)
-                    };
-                })
-            })
-            .entries(data)
+		var nest = d3.nest()
+			.key(mgroup)
+			.rollup(g => {
+				return g.map(d => {
+					return {
+						group: mgroup(d),
+						y: my(d),
+						x: mx(d),
+						label: mlabel(d)
+					};
+				})
+			})
+			.entries(data)
 
-        return nest;
+		return nest;
 
-    })
+	})
 
-
-    var chart = raw.chart()
-        .title('Convex Hull')
-        .description(
-            "In mathematics, the <a href='https://en.wikipedia.org/wiki/Convex_hull'>convex hull</a> is the smallest convex shape containing a set of points. Applied to a scatterplot, it is useful to identify points belonging to the same category.<br /> <br/>Based on <a href='http://bl.ocks.org/mbostock/4341699'>http://bl.ocks.org/mbostock/4341699</a>")
-        .thumbnail("imgs/multipleConvexHull.png")
-        .model(model)
-        .category('Dispersion')
-
-
-    var width = chart.number()
-        .title("Width")
-        .defaultValue(800)
-        .fitToWidth(true);
-
-    var height = chart.number()
-        .title("Height")
-        .defaultValue(600);
-
-    var dotRadius = chart.number()
-        .title("Dots Diameter")
-        .defaultValue(6);
-
-    var useZero = chart.checkbox()
-        .title("set origin at (0,0)")
-        .defaultValue(false);
-
-    var stroke = chart.number()
-        .title("Stroke Width")
-        .defaultValue(32);
-
-    var colors = chart.color()
-        .title("Color scale");
-
-    chart.draw(function (selection, data) {
-
-        var xmin = d3.min(data, function (layer) {
-            return d3.min(layer.values, function (d) {
-                return d.x;
-            });
-        });
-        var xmax = d3.max(data, function (layer) {
-            return d3.max(layer.values, function (d) {
-                return d.x;
-            });
-        });
-        var ymin = d3.min(data, function (layer) {
-            return d3.min(layer.values, function (d) {
-                return d.y;
-            });
-        });
-        var ymax = d3.max(data, function (layer) {
-            return d3.max(layer.values, function (d) {
-                return d.y;
-            });
-        })
-
-        //define the drawing space
-        var bottomPadding = 15;
-        //magic formula, no idea why it works. Anyway it defines the max length of the labels string.
-        var leftPadding = ((Math.log(ymax) / 2.302585092994046) + 1) * 9;
-
-        var w = +width() - stroke() - leftPadding;
-        var h = +height() - stroke() - bottomPadding;
-
-        var x = d3.scale.linear().range([0, w]),
-            y = d3.scale.linear().range([h, 0]);
+	var chart = raw.chart()
+		.title('Convex Hull')
+		.description(
+			"In mathematics, the <a href='https://en.wikipedia.org/wiki/Convex_hull'>convex hull</a> is the smallest convex shape containing a set of points. Applied to a scatterplot, it is useful to identify points belonging to the same category.<br /> <br/>Based on <a href='http://bl.ocks.org/mbostock/4341699'>http://bl.ocks.org/mbostock/4341699</a>")
+		.thumbnail("imgs/multipleConvexHull.png")
+		.model(model)
+		.category('Dispersion')
 
 
-        //set domain according to "origin" variable
+	var width = chart.number()
+		.title("Width")
+		.defaultValue(800)
+		.fitToWidth(true);
 
-        if (useZero()) {
-            x.domain([0, xmax]);
-            y.domain([0, ymax]);
-        } else {
-            x.domain([xmin, xmax]);
-            y.domain([ymin, ymax]);
-        }
+	var height = chart.number()
+		.title("Height")
+		.defaultValue(600);
 
-        //define colors
-        colors.domain(data, function (layer) {
-            return layer.key;
-        });
+	var dotRadius = chart.number()
+		.title("Dots Diameter")
+		.defaultValue(6);
 
-        //@TODO: add x and y axes (copy from scatterPlot.js)
+	var useZero = chart.checkbox()
+		.title("set origin at (0,0)")
+		.defaultValue(false);
 
-        var xAxis = d3.svg.axis().scale(x).tickSize(-h).orient("bottom");
-        var yAxis = d3.svg.axis().scale(y).tickSize(-w).orient("left");
+	var stroke = chart.number()
+		.title("Stroke Width")
+		.defaultValue(32);
 
-        var svg = selection
-            .attr("width", +width())
-            .attr("height", +height())
+	var colors = chart.color()
+		.title("Color scale");
+
+	chart.draw((selection, data) => {
+
+		var xmin = d3.min(data, layer => {
+			return d3.min(layer.value, d => {
+				return d.x;
+			});
+		});
+		var xmax = d3.max(data, layer => {
+			return d3.max(layer.value, d => {
+				return d.x;
+			});
+		});
+		var ymin = d3.min(data, layer => {
+			return d3.min(layer.value, d => {
+				return d.y;
+			});
+		});
+		var ymax = d3.max(data, layer => {
+			return d3.max(layer.value, d => {
+				return d.y;
+			});
+		})
+
+		//define the drawing space
+		var bottomPadding = 15;
+		//magic formula, no idea why it works. Anyway it defines the max length of the labels string.
+		var leftPadding = ((Math.log(ymax) / 2.302585092994046) + 1) * 9;
+
+		var w = +width() - stroke() - leftPadding;
+		var h = +height() - stroke() - bottomPadding;
+
+		var x = d3.scaleLinear().range([0, w]),
+			y = d3.scaleLinear().range([h, 0]);
 
 
-        svg.append("g")
-            .attr("class", "x axis")
-            .style("stroke-width", "1px")
-            .style("font-size", "10px")
-            .style("font-family", "Arial, Helvetica")
-            .attr("transform", "translate(" + (stroke() / 2 + leftPadding) + "," + (height() - stroke() / 2 - bottomPadding) + ")")
-            .call(xAxis);
+		//set domain according to "origin" variable
 
-        svg.append("g")
-            .attr("class", "y axis")
-            .style("stroke-width", "1px")
-            .style("font-size", "10px")
-            .style("font-family", "Arial, Helvetica")
-            .attr("transform", "translate(" + (stroke() / 2 + leftPadding) + "," + stroke() / 2 + ")")
-            .call(yAxis);
+		if(useZero()) {
+			x.domain([0, xmax]);
+			y.domain([0, ymax]);
+		} else {
+			x.domain([xmin, xmax]);
+			y.domain([ymin, ymax]);
+		}
 
-        d3.selectAll(".y.axis line, .x.axis line, .y.axis path, .x.axis path")
-            .style("shape-rendering", "crispEdges")
-            .style("fill", "none")
-            .style("stroke", "#ccc")
+		//define colors
+		colors.domain(data, layer => {
+			return layer.key;
+		});
+
+		//@TODO: add x and y axes (copy from scatterPlot.js)
+
+		var xAxis = d3.axisBottom(x).tickSize(-h);
+		var yAxis = d3.axisLeft(y).tickSize(-w);
+
+		var svg = selection
+			.attr("width", +width())
+			.attr("height", +height())
+
+		svg.append("g")
+			.attr("class", "x axis")
+			.style("stroke-width", "1px")
+			.style("font-size", "10px")
+			.style("font-family", "Arial, Helvetica")
+			.attr("transform", `translate(${stroke() / 2 + leftPadding}, ${height() - stroke() / 2 - bottomPadding})`)
+			.call(xAxis);
+
+		svg.append("g")
+			.attr("class", "y axis")
+			.style("stroke-width", "1px")
+			.style("font-size", "10px")
+			.style("font-family", "Arial, Helvetica")
+			.attr("transform", `translate(${stroke() / 2 + leftPadding}, ${stroke() / 2})`)
+			.call(yAxis);
+
+		d3.selectAll(".y.axis line, .x.axis line, .y.axis path, .x.axis path")
+			.style("shape-rendering", "crispEdges")
+			.style("fill", "none")
+			.style("stroke", "#ccc")
 
 
-        //for each group...
-        data.forEach(function (layer) {
+		//for each group...
+		data.forEach(layer => {
 
-            var vertices = layer.values.map(function (d) {
-                
-                return [x(d.x), y(d.y)]
-            })
+			var vertices = layer.value.map(d => {
+				return [x(d.x), y(d.y)]
+			})
 
-            //assure that there are at least 4 vertices (required by convex hull)
-            //add 0.01 to slightly move from position (otherwise convex hull won't work)
-            //dirty but working.
-            while (vertices.length < 3) {
-                vertices.push([vertices[0][0] + 0.01, vertices[0][1] + 0.01]);
-            }
+			//assure that there are at least 4 vertices (required by convex hull)
+			//add 0.01 to slightly move from position (otherwise convex hull won't work)
+			//dirty but working.
+			while(vertices.length < 3) {
+				vertices.push([vertices[0][0] + 0.01, vertices[0][1] + 0.01]);
+			}
 
-            var g = svg.append("g")
-                .attr("id", layer.key)
-                .attr("transform", "translate(" + (stroke() / 2 + leftPadding) + "," + stroke() / 2 + ")")
+			var g = svg.append("g")
+				.attr("id", layer.key)
+				.attr("transform", `translate(${stroke() / 2 + leftPadding}, ${stroke() / 2})`)
 
-            var gcolor = colors()(layer.key);
+			var gcolor = colors()(layer.key);
 
-            g.append("path")
-                .datum(d3.geom.hull(vertices))
-                .style("fill", gcolor)
-                .style("opacity", 0.3)
-                .style("stroke", gcolor)
-                .style("stroke-width", +stroke())
-                .style("stroke-linejoin", "round")
-                .attr("d", function (d) {
-                    return "M" + d.join("L") + "Z";
-                });
+			g.append("path")
+				.datum(d3.polygonHull(vertices))
+				.style("fill", gcolor)
+				.style("opacity", 0.3)
+				.style("stroke", gcolor)
+				.style("stroke-width", +stroke())
+				.style("stroke-linejoin", "round")
+				.attr("d", d => {
+					return "M" + d.join("L") + "Z";
+				});
 
-            g.selectAll("circle")
-                .data(vertices)
-                .enter().append("circle")
-                .style("fill", gcolor)
-                .attr("r", dotRadius() / 2)
-                .attr("transform", function (d) {
-                    return "translate(" + d + ")";
-                })
-        })
-        
-        // now, add label above all
-        if(mlabel() != null) {
-            var txt_group = svg.append('g')
-                                .attr("transform", "translate(" + (stroke() / 2 + leftPadding) + "," + stroke() / 2 + ")");
-            
-            data.forEach(function (layer) {
+			g.selectAll("circle")
+				.data(vertices)
+				.enter().append("circle")
+				.style("fill", gcolor)
+				.attr("r", dotRadius() / 2)
+				.attr("transform", d => {
+					return `translate(${d})`;
+				})
+		})
 
-                layer.values.forEach(function(item){
-                    txt_group.append("text")
-                        .attr("transform", "translate(" + x(item.x) +"," + y(item.y) + ")")
-                        .attr("text-anchor", "middle")
-                        .style("font-size","10px")
-                        .style("font-family","Arial, Helvetica")
-                        .text(item.label)
+		// now, add label above all
+		if(mlabel() != null) {
+			var txt_group = svg.append('g')
+				.attr("transform", `translate(${stroke() / 2 + leftPadding}, ${stroke() / 2})`)
 
-                    console.log(item);
-                })
-            });
-        }
-    })
+			data.forEach(layer => {
+
+				layer.value.forEach(item => {
+					txt_group.append("text")
+						.attr("transform", `translate(${x(item.x)}, ${y(item.y)})`)
+						.attr("text-anchor", "middle")
+						.style("font-size", "10px")
+						.style("font-family", "Arial, Helvetica")
+						.text(item.label)
+				})
+			});
+		}
+	})
 })();
