da040f8cea53ac67abb7a4329861068016d2b706
mikima <mikimauri+github@gmail.com>
2018-04-04 14:14:29 +0200

    Updated treemap to d3v5

--- charts/treemap.js
+++ charts/treemap.js
@@ -1,13 +1,13 @@
-(function(){
+(function() {
 
 	var tree = raw.models.tree();
 
 	var chart = raw.chart()
-        .title('Treemap')
+		.title('Treemap')
 		.description(
-            "A space filling visualization of data hierarchies and proportion between elements. The different hierarchical levels create visual clusters through the subdivision into rectangles proportionally to each element's value. Treemaps are useful to represent the different proportion of nested hierarchical data structures.<br/>Based on <a href='http://bl.ocks.org/mbostock/4063582'>http://bl.ocks.org/mbostock/4063582</a>")
+			"A space filling visualization of data hierarchies and proportion between elements. The different hierarchical levels create visual clusters through the subdivision into rectangles proportionally to each element's value. Treemaps are useful to represent the different proportion of nested hierarchical data structures.<br/>Based on <a href='http://bl.ocks.org/mbostock/4063582'>http://bl.ocks.org/mbostock/4063582</a>")
 		.thumbnail("imgs/treemap.png")
-	    .category('Hierarchy (weighted)')
+		.category('Hierarchy (weighted)')
 		.model(tree)
 
 	var width = chart.number()
@@ -26,52 +26,64 @@
 	var colors = chart.color()
 		.title("Color scale")
 
-	chart.draw(function (selection, data){
+	chart.draw(function(selection, data) {
 
+		//unknown function
 		var format = d3.format(",d");
 
-		var layout = d3.layout.treemap()
-			.sticky(true)
-            .padding(+padding())
-            .size([+width(), +height()])
-            .value(function(d) { return d.size; })
-
+		// get the drawing area
 		var g = selection
-    	    .attr("width", +width())
-    	    .attr("height", +height())
-    	  	.append("g")
-    	    .attr("transform", "translate(.5,.5)");
+			.attr("width", +width())
+			.attr("height", +height())
+			.append("g")
+			.attr("transform", "translate(.5,.5)");
 
-		var nodes = layout.nodes(data)
-	  	    .filter(function(d) { return !d.children; });
 
-        colors.domain(nodes, function (d){ return d.color; });
+		// create the layout
+		var layout = d3.treemap()
+			.tile(d3.treemapResquarify)
+			.padding(+padding())
+			.size([+width(), +height()])
+			//.sticky(true)
+			//.value(function(d) { return d.size; })
 
+		// create the tree
+		var root = d3.hierarchy(data)
+			.sum(function(d) { return +d.size; });
+
+		// inform the layout
+		layout(root);
+
+
+		// define color scale
+		colors.domain(root.descendants().filter(function(d) { return !d.children }),
+			function(d) { return d.data.color; });
+
+		// Create cells
 		var cell = g.selectAll("g")
-    	    .data(nodes)
-    	    .enter().append("g")
-    	    .attr("class", "cell")
-    	    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+			.data(root.leaves())
+			.enter().append("g")
+			.attr("class", "cell")
+			.attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; });
+		
+		cell.append("rect")
+			.attr("width", function(d) { return d.x1 - d.x0; })
+			.attr("height", function(d) { return d.y1 - d.y0; })
+			.style("fill", function(d) { return colors()(d.data.color); })
+			//.style("fill-opacity", function(d) { return d.children ? 0 : 1; })
+			.style("stroke", "#fff")
 
-		cell.append("svg:rect")
-    	    .attr("width", function (d) { return d.dx; })
-    	    .attr("height", function (d) { return d.dy; })
-    	    .style("fill", function (d) { return colors()(d.color); })
-    	    .style("fill-opacity", function (d) {  return d.children ? 0 : 1; })
-			.style("stroke","#fff")
-
-		cell.append("svg:title")
+		cell.append("title")
 			.text(function(d) { return d.name + ": " + format(d.size); });
 
-		cell.append("svg:text")
-    	    .attr("x", function(d) { return d.dx / 2; })
-    	    .attr("y", function(d) { return d.dy / 2; })
-    	    .attr("dy", ".35em")
-    	    .attr("text-anchor", "middle")
-	  //  .attr("fill", function (d) { return raw.foreground(color()(d.color)); })
-    	   	.style("font-size","11px")
-    		.style("font-family","Arial, Helvetica")
-    	    .text(function(d) { return d.label ? d.label.join(", ") : d.name; });
+		cell.append("text")
+			.attr("x", function(d) { return (d.x1 - d.x0) / 2; })
+			.attr("y", function(d) { return (d.y1 - d.y0) / 2; })
+			.attr("dy", ".35em")
+			.attr("text-anchor", "middle")
+			.style("font-size", "11px")
+			.style("font-family", "Arial, Helvetica")
+			.text(function(d) { return d.data.label ? d.data.label.join(", ") : d.data.name; });
 
 	})
 })();
