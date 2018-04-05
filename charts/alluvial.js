b361200b0cad6e1cb92cb6ca0e71bef9c929af85
mikima <mikimauri+github@gmail.com>
2018-04-05 14:55:58 +0200

    Updated alluvial
    Should fix also #153

--- charts/alluvial.js
+++ charts/alluvial.js
@@ -5,7 +5,7 @@
 	var chart = raw.chart()
 		.title('Alluvial Diagram')
 		.description(
-			"Alluvial diagrams allow to represent flows and to see correlations between categorical dimensions, visually linking to the number of elements sharing the same categories. It is useful to see the evolution of cluster (such as the number of people belonging to a specific group). It can also be used to represent bipartite graphs, using each node group as dimensions.<br/>Mainly based on DensityDesign's work with Fineo, it is inspired by <a href='http://bost.ocks.org/mike/sankey/'>http://bost.ocks.org/mike/sankey/</a>")
+			"Alluvial diagrams allow to represent flows and to see correlations between categorical dimensions, visually linking to the number of elements sharing the same categories. It is useful to see the evolution of cluster (such as the number of people belonging to a specific group). It can also be used to represent bipartite graphs, using each node group as dimensions.<br/>Mainly based on DensityDesign's work with Fineo, it is inspired by <a href='https://bl.ocks.org/mbostock/ca9a0bb7ba204d12974bca90acc507c0'>https://bl.ocks.org/mbostock/ca9a0bb7ba204d12974bca90acc507c0</a>")
 		.thumbnail("imgs/alluvial.png")
 		.category("Multi categorical")
 		.model(graph);
@@ -23,6 +23,10 @@
 		.title("Node Width")
 		.defaultValue(5);
 
+	var opacity = chart.number()
+		.title("Links opacity")
+		.defaultValue(.4);
+
 	var sortBy = chart.list()
 		.title("Sort by")
 		.values(['size', 'name', 'automatic'])
@@ -33,165 +37,146 @@
 
 	chart.draw((selection, data) => {
 
-		var formatNumber = d3.format(",.0f"),
-			format = d => {
-				return formatNumber(d);
-			};
-
+		// get the drawing area
 		var g = selection
 			.attr("width", +width())
 			.attr("height", +height() + 20)
 			.append("g")
 			.attr("transform", "translate(0, 10)");
 
-		// Calculating the best nodePadding
+		// define numbers formatting
+		var formatNumber = d3.format(",.0f"),
+			format = function(d) { return formatNumber(d); };
 
+		// Calculating the best nodePadding (TODO: improve)
 		var nested = d3.nest()
-			.key(d => {
-				return d.group;
-			})
-			.rollup(d => {
-				return d.length;
-			})
+			.key(function(d) { return d.group; })
+			.rollup(function(d) { return d.length; })
 			.entries(data.nodes);
 
-		var maxNodes = d3.max(nested, d => {
-			return d.values;
-		});
+		var maxNodes = d3.max(nested, function(d) { return d.values; });
+		var bestPadding = d3.min([10, (height() - maxNodes) / maxNodes])
 
+		// create sankey object
 		var sankey = d3.sankey()
 			.nodeWidth(+nodeWidth())
-			.nodePadding(d3.min([10, (height() - maxNodes) / maxNodes]))
+			.nodePadding(bestPadding)
 			.size([+width(), +height()]);
 
-		var path = sankey.link(),
-			nodes = data.nodes,
-			links = data.links;
+		// use the loaded data
+		sankey(data);
 
-		sankey
-			.nodes(nodes)
-			.links(links)
-			.layout(32);
+		// define colors
+		colors.domain(data.links, function(d) { return d.source.group + " - " + d.source.name; });
+
+		// add values to nodes
+		data.nodes.forEach(function(d) {
+			// get height for each node
+			d.dx = d.x1 - d.x0;
+			d.dy = d.y1 - d.y0;
+			// check if the name is a number
+
+			if(!isNaN(+d.name)) {
+				d.name = +d.name;
+			}
+		})
 
 		// Re-sorting nodes
+		var nested = d3.nest()
+			.key(function(d) { return d.group; })
+			.entries(data.nodes)
 
-		nested = d3.nest()
-			.key(d => {
-				return d.group;
-			})
-			.map(nodes);
+		nested
+			.forEach(function(d) {
 
-		Object.values(nested)
-			.forEach(d => {
-				var y = (height() - d3.sum(d, n => {
-					return n.dy + sankey.nodePadding();
-				})) / 2 + sankey.nodePadding() / 2;
-				d.sort((a, b) => {
-					if(sortBy() == "automatic") return b.y - a.y;
-					if(sortBy() == "size") return b.dy - a.dy;
-					if(sortBy() == "name") return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+				var y = (height() - d3.sum(d.values, function(n) { return n.dy + sankey.nodePadding(); })) / 2 + sankey.nodePadding() / 2;
+				
+				d.values.sort(function(a, b) {
+					if (sortBy() == "automatic") return b.y0 - a.y0;
+					if (sortBy() == "size") return b.dy - a.dy;
+					//if (sortBy() == "name") return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+					if(sortBy() == "name") {
+						var a1 = typeof a.name, b1 = typeof b.name;
+  						return a1 < b1 ? -1 : a1 > b1 ? 1 : a.name < b.name ? -1 : a.name > b.name ? 1 : 0;}
 				})
-				d.forEach(node => {
-					node.y = y;
+
+				d.values.forEach(function(node) {
+					node.y0 = y;
 					y += node.dy + sankey.nodePadding();
 				})
-			});
+			})
 
 		// Resorting links
 
-		Object.values(nested).forEach(d => {
+		nested.forEach(function(d) {
 
-			d.forEach(node => {
+			d.values.forEach(function(node) {
 
-				var ly = 0;
+				var ly = node.y0;
+
 				node.sourceLinks
-					.sort((a, b) => {
-						return a.target.y - b.target.y;
+					.sort(function(a, b) {
+						return a.target.y0 - b.target.y0;
 					})
-					.forEach(link => {
-						link.sy = ly;
-						ly += link.dy;
-					});
+					.forEach(function(link) {
+						link.y0 = ly + link.width / 2;
+						ly += link.width;
+					})
 
-				ly = 0;
+				ly = node.y0;
 
 				node.targetLinks
-					.sort((a, b) => {
-						return a.source.y - b.source.y;
+					.sort(function(a, b) {
+						return a.source.y0 - b.source.y0;
 					})
-					.forEach(link => {
-						link.ty = ly;
-						ly += link.dy;
-					});
+					.forEach(function(link) {
+						link.y1 = ly + link.width / 2;
+						ly += link.width;
+					})
 			})
 		})
 
-		colors.domain(links, d => {
-			return d.source.name;
-		});
-
-		var link = g.append("g").selectAll(".link")
-			.data(links)
+		//prepare link
+		var link = g.append("g")
+			.attr("class", "links")
+			.attr("fill", "none")
+			.attr("stroke-opacity", +opacity())
+			.selectAll("path")
+			.data(data.links)
 			.enter().append("path")
-			.attr("class", "link")
-			.attr("d", path)
-			.style("stroke-width", d => {
-				return Math.max(1, d.dy);
-			})
-			.style("fill", "none")
-			.style("stroke", d => {
-				return colors()(d.source.name);
-			})
-			.style("stroke-opacity", ".4")
-			.sort((a, b) => {
-				return b.dy - a.dy;
-			})
-			.append("title")
-			.text(d => {
-				return d.value;
-			});
+			.attr("d", d3.sankeyLinkHorizontal())
+			.style("stroke", function(d){return colors()(d.source.group + " - " + d.source.name); })
+			.attr("stroke-width", function(d) { return d.width; });
 
-		var node = g.append("g").selectAll(".node")
-			.data(nodes)
-			.enter().append("g")
-			.attr("class", "node")
-			.attr("transform", d => {
-				return `translate(${d.x}, ${d.y})`;
-			});
 
+		//prepare node
+		var node = g.append("g")
+			.attr("class", "nodes")
+			.attr("font-family", "sans-serif")
+			.attr("font-size", 10)
+			.selectAll("g")
+			.data(data.nodes)
+			.enter().append("g");
+
+		//add rectangle
 		node.append("rect")
-			.attr("height", d => {
-				return d.dy;
-			})
-			.attr("width", sankey.nodeWidth())
-			.style("fill", d => {
-				return d.sourceLinks.length ? colors(d.name) : "#666";
-			})
-			.append("title")
-			.text(d => {
-				return d.name + "\n" + format(d.value);
-			});
+			.attr("x", function(d) { return d.x0; })
+			.attr("y", function(d) { return d.y0; })
+			.attr("height", function(d) { return d.dy; })
+			.attr("width", function(d) { return d.dx; })
+			.attr("fill", function(d) { return '#000' });
 
+
+		//add labels
 		node.append("text")
-			.attr("x", -6)
-			.attr("y", d => {
-				return d.dy / 2;
-			})
-			.attr("dy", ".35em")
+			.attr("x", function(d) { return d.x0 - 6; })
+			.attr("y", function(d) { return d.y0 + d.dy / 2; })
+			.attr("dy", "0.35em")
 			.attr("text-anchor", "end")
-			.attr("transform", null)
-			.text(d => {
-				return d.name;
-			})
-			.style("font-size", "11px")
-			.style("font-family", "Arial, Helvetica")
-			.style("pointer-events", "none")
-			.filter(d => {
-				return d.x < +width() / 2;
-			})
-			.attr("x", 6 + sankey.nodeWidth())
+			.text(function(d) { return d.name; })
+			.filter(function(d) { return d.x0 < nodeWidth() })
+			.attr("x", function(d) { return d.x1 + 6; })
 			.attr("text-anchor", "start");
-
 	})
 
 })();
