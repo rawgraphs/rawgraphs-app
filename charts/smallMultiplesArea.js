91b380864972ac2c8952150d03f55099b475530a
mikima <mikimauri+github@gmail.com>
2018-04-07 21:53:30 +0200

    Updated SmallMultiplesArea to d3v5

--- charts/smallMultiplesArea.js
+++ charts/smallMultiplesArea.js
@@ -1,211 +1,239 @@
-(function(){
+(function() {
 
-    var stream = raw.model();
+	var stream = raw.model();
 
-    var group = stream.dimension()
-        .title('Group')
-        .required(1)
+	var group = stream.dimension()
+		.title('Group')
+		.required(1)
 
-    var date = stream.dimension()
-        .title('Date')
-        .types(Date)
-        .accessor(function (d){ return this.type() == "Date" ? Date.parse(d) : +d; })
-        .required(1)
+	var date = stream.dimension()
+		.title('Date')
+		.types(Date)
+		.accessor(function(d) { return this.type() == "Date" ? Date.parse(d) : +d; })
+		.required(1)
 
-    var size = stream.dimension()
-        .title('Size')
-        .types(Number)
-        .required(1)
+	var size = stream.dimension()
+		.title('Size')
+		.types(Number)
+		.required(1)
 
-    stream.map(function (data){
-        if (!group()) return [];
+	stream.map(function(data) {
+		if (!group()) return [];
 
-        var dates = d3.set(data.map(function (d){ return +date(d); })).values();
+		var dates = d3.set(data.map(function(d) { return +date(d); })).values();
 
-        var groups = d3.nest()
-            .sortKeys(function(d){ return 1})
-            .key(group)
-            .rollup(function (g){
-                var singles = d3.nest()
-                    .key(function(d){ return +date(d); })
-                    .rollup(function (d){
-                        return {
-                            group : group(d[0]),
-                            date : date(d[0]),
-                            size : size() ? d3.sum(d,size) : d.length
-                        }
-                    })
-                    .map(g);
+		var results = d3.nest()
+			.key(function(d) { return d[group()] })
+			.key(function(d) { return d[date()] }).sortKeys(d3.ascending)
+			.rollup(function(v) {
+				return {
+					size: !size() ? v.length : d3.sum(v, function(e) { return size(e) }),
+					date: date(v[0]),
+					group: group(v[0])
+				}
+			})
+			.entries(data);
 
-                return d3.values(singles);
-            })
-            .map(data)
+		// remap the array
+		results.forEach(function(d) { d.values = d.values.map(function(item) { return item.value }) });
 
-        return d3.values(groups).map(function(d){ return d.sort(function(a,b){ return a.date - b.date; }) });
+		return results;
+	})
 
-    })
+	var chart = raw.chart()
+		.title('Area graph')
+		.thumbnail("imgs/smallMultiples.png")
+		.description("A small multiple is a series of small similar graphics or charts, allowing them to be easily compared.<br/>Based on <a href='http://bl.ocks.org/mbostock/9490313'>http://bl.ocks.org/mbostock/9490313</a>")
+		.category('Time series')
+		.model(stream)
 
-    var chart = raw.chart()
-        .title('Area graph')
-        .thumbnail("imgs/smallMultiples.png")
-        .description("A small multiple is a series of small similar graphics or charts, allowing them to be easily compared.<br/>Based on <a href='http://bl.ocks.org/mbostock/9490313'>http://bl.ocks.org/mbostock/9490313</a>")
-        .category('Time series')
-        .model(stream)
+	var width = chart.number()
+		.title("Width")
+		.defaultValue(1000)
+		.fitToWidth(true)
 
-    var width = chart.number()
-        .title("Width")
-        .defaultValue(1000)
-        .fitToWidth(true)
+	var height = chart.number()
+		.title("Height")
+		.defaultValue(500)
 
-    var height = chart.number()
-        .title("Height")
-        .defaultValue(500)
+	var padding = chart.number()
+		.title("Padding")
+		.defaultValue(5)
 
-    var padding = chart.number()
-        .title("Padding")
-        .defaultValue(5)
+	var scale = chart.checkbox()
+		.title("Use same scale")
+		.defaultValue(false)
 
-    var scale = chart.checkbox()
-        .title("Use same scale")
-        .defaultValue(false)
+	var specular = chart.checkbox()
+		.title("Center values vertically")
+		.defaultValue(false)
 
-    var specular = chart.checkbox()
-        .title("Center values vertically")
-        .defaultValue(false)
+	var colors = chart.color()
+		.title("Color scale")
 
-    var colors = chart.color()
-        .title("Color scale")
-    
-    var curve = chart.list()
-      .title("Interpolation")
-      .values(['Basis spline','Sankey','Linear'])
-      .defaultValue('Basis spline')
-    
-    var sorting = chart.list()
-        .title("Sort by")
-        .values(['Original','Total (descending)', 'Total (ascending)', 'Name'])
-        .defaultValue('Original')
+	var curve = chart.list()
+		.title("Interpolation")
+		.values(['Cardinal', 'Basis spline', 'Sankey', 'Linear'])
+		.defaultValue('Cardinal')
 
-    chart.draw(function (selection, data){
-        
-        //sort data
-        function sortBy(a,b){
-            if (sorting() == 'Total (descending)'){                
-                return a.reduce(function (c,d) {return c + d.size}, 0) - b.reduce(function (c,d) {return c + d.size}, 0)
-            }
-            if (sorting() == 'Total (ascending)') return b.reduce(function (c,d) {return c + d.size}, 0) - a.reduce(function (c,d) {return c + d.size}, 0);
-            if (sorting() == 'Name'){
-                if(a[0].group < b[0].group) return -1;
-                if(a[0].group > b[0].group) return 1;
-            }
-        }
-        
-        data.sort(sortBy);
-        
-        console.log(data);
-        
-        var curves = {
-            'Basis spline' : 'basis',
-            'Sankey' : interpolate,
-            'Linear' : 'linear'
-            }
+	var sorting = chart.list()
+		.title("Sort by")
+		.values(['Original', 'Total (descending)', 'Total (ascending)', 'Name'])
+		.defaultValue('Original')
 
-        var w = +width(),
-            h = (+height()-20 - (+padding()*(data.length-1))) / data.length;
+	// interpoaltion function
 
-        var svg = selection
-            .attr("width", +width())
-            .attr("height", +height())
+	function CurveSankey(context) {
+			this._context = context;
+		}
 
-        var x = d3.time.scale()
-            .range([0, w]);
+		CurveSankey.prototype = {
+			areaStart: function() {
+				this._line = 0;
+			},
+			areaEnd: function() {
+				this._line = NaN;
+			},
+			lineStart: function() {
+				this._x = this._y = NaN;
+				this._point = 0;
+			},
+			lineEnd: function() {
+				if (this._line || (this._line !== 0 && this._point === 1)) this._context.closePath();
+				this._line = 1 - this._line;
+			},
+			point: function(x, y) {
+				x = +x, y = +y;
+				switch (this._point) {
+					case 0:
+						this._point = 1;
+						this._line ? this._context.lineTo(x,y) : this._context.moveTo(x, y);
+						break;
+					case 1:
+						this._point = 2; // proceed
+					default:
+						var mx = (x - this._x) / 2 + this._x;
+						this._context.bezierCurveTo(mx, this._y, mx, y, x, y);
+						break;
+				}
+				this._x = x, this._y = y;
+			}
+		};
 
-        var y = d3.scale.linear()
-            .range([h, 0]);
+		var curveSankey = function(context) {
+			return new CurveSankey(context);
+		}
 
-        var area = d3.svg.area()
-            .x(function(d) { return x(d.date); })
-            .interpolate(curves[curve()])
+	chart.draw(function(selection, data) {
 
-        //@TODO: expose this in options
-        //var specular = false;
-        // if specular
-        if(specular() == true) {
-            area.y0(function(d) { return h-y(d.size)/2; })
-                .y1(function(d) { return y(d.size)/2; })  
-        } else {
-            area.y0(h) //align to baseline
-                .y1(function(d) { return y(d.size); })
-        }
+		//sort data
+		function sortBy(a, b) {
+			if (sorting() == 'Total (descending)') {
+				return a.reduce(function(c, d) { return c + d.size }, 0) - b.reduce(function(c, d) { return c + d.size }, 0)
+			}
+			if (sorting() == 'Total (ascending)') return b.reduce(function(c, d) { return c + d.size }, 0) - a.reduce(function(c, d) { return c + d.size }, 0);
+			if (sorting() == 'Name') {
+				if (a[0].group < b[0].group) return -1;
+				if (a[0].group > b[0].group) return 1;
+			}
+		}
 
-        x.domain([
-            d3.min(data, function(layer) { return d3.min(layer, function(d) { return d.date; }); }),
-            d3.max(data, function(layer) { return d3.max(layer, function(d) { return d.date; }); })
-        ])
+		data.sort(sortBy);
 
-        colors.domain(data, function (d){ return d[0].group; })
+		var curves = {
+			'Basis spline': d3.curveBasis,
+			'Cardinal': d3.curveCardinal,
+			'Sankey': curveSankey,
+			'Linear': d3.curveLinear
+		}
 
-        var xAxis = d3.svg.axis().scale(x).tickSize(-height()+15).orient("bottom");
+		var w = +width(),
+			h = (+height() - 20 - (+padding() * (data.length - 1))) / data.length;
 
-        svg.append("g")
-            .attr("class", "x axis")
-            .style("stroke-width", "1px")
-            .style("font-size","10px")
-            .style("font-family","Arial, Helvetica")
-            .attr("transform", "translate(" + 0 + "," + (height()-15) + ")")
-            .call(xAxis);
+		var svg = selection
+			.attr("width", +width())
+			.attr("height", +height())
 
-        d3.selectAll(".x.axis line, .x.axis path")
-            .style("shape-rendering","crispEdges")
-            .style("fill","none")
-            .style("stroke","#ccc")
+		var x = d3.scaleTime()
+			.range([0, w]);
+
+		var y = d3.scaleLinear()
+			.range([h, 0]);
+
+		var area = d3.area()
+			.x(function(d) { return x(d.date); })
+			//.y0(h)
+			//.y1(function(d) { return y(+d.size) })
+			.curve(curves[curve()]);
+
+		//@TODO: expose this in options
+		//var specular = false;
+		// if specular
+		if (specular()) {
+			area.y0(function(d) { return h - y(d.size) / 2; })
+				.y1(function(d) { return y(d.size) / 2; })
+		} else {
+			area.y0(h) //align to baseline
+				.y1(function(d) { return y(d.size); })
+		}
+
+		x.domain([
+			d3.min(data, function(layer) { return d3.min(layer.values, function(d) { return d.date; }); }),
+			d3.max(data, function(layer) { return d3.max(layer.values, function(d) { return d.date; }); })
+		])
+
+		colors.domain(data, function(d) { return d.key; })
+
+		var xAxis = d3.axisBottom(x).tickSize(-height() + 15);
+
+		svg.append("g")
+			.attr("class", "x axis")
+			.style("stroke-width", "1px")
+			.style("font-size", "10px")
+			.style("font-family", "Arial, Helvetica")
+			.attr("transform", "translate(" + 0 + "," + (height() - 15) + ")")
+			.call(xAxis);
+
+		d3.selectAll(".x.axis line, .x.axis path")
+			.style("shape-rendering", "crispEdges")
+			.style("fill", "none")
+			.style("stroke", "#ccc")
 
 
-        svg.selectAll("g.flow")
-            .data(data)
-            .enter().append("g")
-            .attr("class","flow")
-            .attr("title", function(d) { return d[0].group; })
-            .attr("transform", function(d,i) { return "translate(0," + ((h+padding())*i) + ")"})
-            .each(multiple);
+		svg.selectAll("g.flow")
+			.data(data)
+			.enter().append("g")
+			.attr("class", "flow")
+			.attr("title", function(d) { return d.key; })
+			.attr("transform", function(d, i) { return "translate(0," + ((h + padding()) * i) + ")" })
+			.each(multiple);
 
-        svg.selectAll("g.flow")
-            .append("text")
-            .attr("x", w - 6)
-            .attr("y", h - 6)
-            .style("font-size","10px")
-            .style("fill", function(d){ return raw.foreground(colors()(d[0].group)) })
-            .style("font-family","Arial, Helvetica")
-            .style("text-anchor", "end")
-            .text(function(d) { return d[0].group; });
+		svg.selectAll("g.flow")
+			.append("text")
+			.attr("x", w - 6)
+			.attr("y", h - 6)
+			.style("font-size", "10px")
+			.style("fill", function(d) { return colors()(d.key) })
+			.style("font-family", "Arial, Helvetica")
+			.style("text-anchor", "end")
+			.text(function(d) { return d.key; });
 
-        function multiple(single) {
+		function multiple(single) {
 
-            var g = d3.select(this);
+			var g = d3.select(this);
 
-            if (scale()) y.domain([0, d3.max(data, function(layer) { return d3.max(layer, function(d) { return d.size; }); })])
-            else y.domain([0, d3.max(single, function(d) { return d.size; })]);
+			if (scale()) {
+				y.domain([0, d3.max(data, function(layer) { return d3.max(layer.values, function(d) { return d.size; }); })])
+			} else {
+				y.domain([0, d3.max(single.values, function(d) { return d.size; })]);
+			}
 
-            g.append("path")
-              .attr("class", "area")
-              .style("fill", function(d){ return colors()(d[0].group); })
-              .attr("d", area(single));
-        }
-        
-        function interpolate(points) {
-          var x0 = points[0][0], y0 = points[0][1], x1, y1, x2,
-              path = [x0, ",", y0],
-              i = 0,
-              n = points.length;
+			g.append("path")
+				.attr("class", "area")
+				.style("fill", function(d) { return colors()(d.key); })
+				.attr("d", area(single.values));
+		}
 
-          while (++i < n) {
-            x1 = points[i][0], y1 = points[i][1], x2 = (x0 + x1) / 2;
-            path.push("C", x2, ",", y0, " ", x2, ",", y1, " ", x1, ",", y1);
-            x0 = x1, y0 = y1;
-          }
-          return path.join("");
-        }
-
-    })
+	})
 
 })();
