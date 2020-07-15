c0cd50a0bd6f3f2c309c7a1d39aa777d9c95e183
Mauro Bianchi <bianchimro@gmail.com>
2020-07-16 00:11:23 +0200

    options groups

--- src/components/ChartOptions/ChartOptions.js
+++ src/components/ChartOptions/ChartOptions.js
@@ -1,51 +1,68 @@
-import React, { useMemo } from "react"
-import { baseOptions } from "@raw-temp/rawgraphs-core"
-import ChartOptionNumber from "./ChartOptionTypes/ChartOptionNumber"
-import ChartOptionText from "./ChartOptionTypes/ChartOptionText"
-import ChartOptionColor from "./ChartOptionTypes/ChartOptionColor"
-import ChartOptionColorScale from "./ChartOptionTypes/ChartOptionColorScale"
-import ChartOptionBoolean from "./ChartOptionTypes/ChartOptionBoolean"
-import { map } from "lodash"
+import React, { useMemo } from "react";
+import { baseOptions } from "@raw-temp/rawgraphs-core";
+import ChartOptionNumber from "./ChartOptionTypes/ChartOptionNumber";
+import ChartOptionText from "./ChartOptionTypes/ChartOptionText";
+import ChartOptionColor from "./ChartOptionTypes/ChartOptionColor";
+import ChartOptionColorScale from "./ChartOptionTypes/ChartOptionColorScale";
+import ChartOptionBoolean from "./ChartOptionTypes/ChartOptionBoolean";
+import { map } from "lodash";
+import groupBy from "lodash/groupBy";
 
 const CHART_OPTION_COMPONENTS = {
-  'number': ChartOptionNumber,
-  'text': ChartOptionText,
-  'color': ChartOptionColor,
-  'colorScale': ChartOptionColorScale,
-  'boolean': ChartOptionBoolean
-}
+  number: ChartOptionNumber,
+  text: ChartOptionText,
+  color: ChartOptionColor,
+  colorScale: ChartOptionColorScale,
+  boolean: ChartOptionBoolean,
+};
 
 const ChartOptions = ({ chart, visualOptions, setVisualOptions, error }) => {
-
-  const optionsDefinitions = useMemo(() => {
-    return {
+  const optionsDefinitionsByGroup = useMemo(() => {
+    const options = {
       ...baseOptions,
-      ...chart?.visualOptions
-    }
-  }, [chart])
+      ...chart?.visualOptions,
+    };
+
+    return Object.keys(options).reduce((acc, optionId) => {
+      const option = options[optionId];
+      const group = option?.group || "";
+      if (!acc[group]) {
+        acc[group] = {};
+      }
+      acc[group][optionId] = option;
+      return acc;
+    }, {});
+  }, [chart]);
 
   return (
     <div>
-      {map(optionsDefinitions, (def, optionId) => {
-        const Component = CHART_OPTION_COMPONENTS[def.type]
-
+      {map(optionsDefinitionsByGroup, (options, groupName) => {
         return (
-          <Component
-            key={optionId}
-            {...def}
-            error={error?.errors?.[optionId]}
-            value={visualOptions?.[optionId]}
-            onChange={nextValue => {
-              setVisualOptions({
-                ...visualOptions,
-                [optionId]: nextValue
-              })
-            }}
-          />
-        )
+          <div key={groupName}>
+            <h2>{groupName}</h2>
+            {map(options, (def, optionId) => {
+              const Component = CHART_OPTION_COMPONENTS[def.type];
+
+              return (
+                <Component
+                  key={optionId}
+                  {...def}
+                  error={error?.errors?.[optionId]}
+                  value={visualOptions?.[optionId]}
+                  onChange={(nextValue) => {
+                    setVisualOptions({
+                      ...visualOptions,
+                      [optionId]: nextValue,
+                    });
+                  }}
+                />
+              );
+            })}
+          </div>
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default ChartOptions
+export default ChartOptions;
