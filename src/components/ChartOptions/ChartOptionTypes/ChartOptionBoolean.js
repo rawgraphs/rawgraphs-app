b8ad4a1e5f0fc3b81cc6cd6c5941666ec6038970
Mauro Bianchi <bianchimro@gmail.com>
2020-07-16 01:09:38 +0200

    ChartOptionBoolean implementation, updated charts lib

--- src/components/ChartOptions/ChartOptionTypes/ChartOptionBoolean.js
+++ src/components/ChartOptions/ChartOptionTypes/ChartOptionBoolean.js
@@ -1,7 +1,18 @@
 import React from "react"
 
-const ChartOptionBoolean = () => {
-  return null
+const ChartOptionBoolean = ({label, value, error, onChange}) => {
+  return <label className="d-block">
+  <b>{label}</b><br />
+  <input
+    type="checkbox"
+    checked={value}
+    onChange={e => {
+      onChange(e.target.checked)
+    }}
+    
+  />
+  {error && <small><i>{error}</i></small>}
+</label>
 }
 
 export default ChartOptionBoolean
