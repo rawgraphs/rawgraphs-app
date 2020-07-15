055eeb70d60abe9d61b64bd3e2d5f9d13e0fba22
Mauro Bianchi <bianchimro@gmail.com>
2020-07-15 23:48:11 +0200

    grid layout for ChartPreviewWithOptions

--- src/components/ChartPreviewWIthOptions/ChartPreviewWithOptions.js
+++ src/components/ChartPreviewWIthOptions/ChartPreviewWithOptions.js
@@ -1,28 +1,40 @@
-import React, { useState } from "react"
-import ChartOptions from "../ChartOptions"
-import ChartPreview from "../ChartPreview"
+import React, { useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import ChartOptions from "../ChartOptions";
+import ChartPreview from "../ChartPreview";
 
-const ChartPreviewWithOptions = ({ chart, dataset, dataTypes, mapping, visualOptions, setVisualOptions }) => {
-  const [error, setError] = useState(null)
+const ChartPreviewWithOptions = ({
+  chart,
+  dataset,
+  dataTypes,
+  mapping,
+  visualOptions,
+  setVisualOptions,
+}) => {
+  const [error, setError] = useState(null);
   return (
-    <div>
-      <ChartOptions
-        chart={chart}
-        visualOptions={visualOptions}
-        setVisualOptions={setVisualOptions}
-        error={error}
-      />
-      <ChartPreview 
-        chart={chart}
-        dataset={dataset}
-        dataTypes={dataTypes}
-        mapping={mapping}
-        visualOptions={visualOptions}
-        error={error}
-        setError={setError}
-      />
-    </div>
-  )
-}
+    <Row>
+      <Col xs={3}>
+        <ChartOptions
+          chart={chart}
+          visualOptions={visualOptions}
+          setVisualOptions={setVisualOptions}
+          error={error}
+        />
+      </Col>
+      <Col>
+        <ChartPreview
+          chart={chart}
+          dataset={dataset}
+          dataTypes={dataTypes}
+          mapping={mapping}
+          visualOptions={visualOptions}
+          error={error}
+          setError={setError}
+        />
+      </Col>
+    </Row>
+  );
+};
 
-export default ChartPreviewWithOptions
+export default ChartPreviewWithOptions;
