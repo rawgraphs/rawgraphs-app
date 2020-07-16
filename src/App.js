1d253335946eab24bae37efdf1fe76a45518ae97
Mauro Bianchi <bianchimro@gmail.com>
2020-07-16 10:59:35 +0200

    avoiding useEffect for getting default options

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useCallback, useEffect } from "react";
-import { getOptionsConfig, getDefaultOptionsValues } from "@raw-temp/rawgraphs-core";
+import {
+  getOptionsConfig,
+  getDefaultOptionsValues,
+} from "@raw-temp/rawgraphs-core";
 
-import HeaderItems from './HeaderItems';
+import HeaderItems from "./HeaderItems";
 import Header from "./components/Header";
 import Section from "./components/Section";
 import Footer from "./components/Footer";
@@ -9,39 +12,29 @@ import Footer from "./components/Footer";
 import DataLoader from "./components/DataLoader";
 import charts from "./charts";
 import ChartSelector from "./components/ChartSelector";
-import DataMapping from './components/DataMapping';
+import DataMapping from "./components/DataMapping";
 import ChartPreviewWithOptions from "./components/ChartPreviewWIthOptions";
 
 // #TODO: i18n
 
 function App() {
-  const [data, setData] = useState(null)
-  const [currentChart, setCurrentChart] = useState(charts[0])
-  const [mapping, setMapping] = useState({})
-  const [visualOptions, setVisualOptions] = useState({})
+  const [data, setData] = useState(null);
+  const [currentChart, setCurrentChart] = useState(charts[0]);
+  const [mapping, setMapping] = useState({});
+  const [visualOptions, setVisualOptions] = useState({});
 
-  const handleChartChange = useCallback(nextChart => {
-    setCurrentChart(nextChart)
-    setMapping({})
-  }, [])
-
-  useEffect(() => {
-    if(!currentChart){
-      setVisualOptions({})
-    } else {
-      const options = getOptionsConfig(currentChart?.visualOptions)
-      setVisualOptions(getDefaultOptionsValues(options))
-    }
-  }, [currentChart])
+  const handleChartChange = useCallback((nextChart) => {
+    setCurrentChart(nextChart);
+    setMapping({});
+    const options = getOptionsConfig(nextChart?.visualOptions);
+    setVisualOptions(getDefaultOptionsValues(options));
+  }, []);
 
   return (
     <div className="App">
       <Header menuItems={HeaderItems} />
       <Section title="1. Load your data">
-        <DataLoader
-          data={data}
-          setData={setData}
-        />
+        <DataLoader data={data} setData={setData} />
       </Section>
       {data && (
         <Section title="2. Choose a chart">
@@ -54,10 +47,10 @@ function App() {
       )}
       {data && currentChart && (
         <Section title="3. Mapping">
-          <DataMapping 
+          <DataMapping
             dimensions={currentChart.dimensions}
             dataTypes={data.dataTypes}
-            mapping={mapping} 
+            mapping={mapping}
             setMapping={setMapping}
           />
         </Section>
@@ -74,15 +67,9 @@ function App() {
           />
         </Section>
       )}
-      <Section title="5. Export">
-        Export here
-        </Section>
-      <Section title="0. Typography">
-        {typography}
-      </Section>
-      <Footer>
-        Footer items go here!
-        </Footer>
+      <Section title="5. Export">Export here</Section>
+      <Section title="0. Typography">{typography}</Section>
+      <Footer>Footer items go here!</Footer>
     </div>
   );
 }
@@ -91,16 +78,30 @@ export default App;
 
 const typography = (
   <>
-    <h1>h1. Bootstrap heading <small>Secondary text in heading</small></h1>
-    <h2>h2. Bootstrap heading <small>Secondary text in heading</small></h2>
-    <h3>h3. Bootstrap heading <small>Secondary text in heading</small></h3>
-    <h4>h4. Bootstrap heading <small>Secondary text in heading</small></h4>
-    <h5>h5. Bootstrap heading <small>Secondary text in heading</small></h5>
-    <h6>h6. Bootstrap heading <small>Secondary text in heading</small></h6>
-    <p className="lead">Lead Paragraph. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+    <h1>
+      h1. Bootstrap heading <small>Secondary text in heading</small>
+    </h1>
+    <h2>
+      h2. Bootstrap heading <small>Secondary text in heading</small>
+    </h2>
+    <h3>
+      h3. Bootstrap heading <small>Secondary text in heading</small>
+    </h3>
+    <h4>
+      h4. Bootstrap heading <small>Secondary text in heading</small>
+    </h4>
+    <h5>
+      h5. Bootstrap heading <small>Secondary text in heading</small>
+    </h5>
+    <h6>
+      h6. Bootstrap heading <small>Secondary text in heading</small>
+    </h6>
+    <p className="lead">
+      Lead Paragraph. Vivamus sagittis lacus vel augue laoreet rutrum faucibus
+      dolor auctor.
+    </p>
     <p>An ordinary paragraph.</p>
     <p className="lighter">Paragraph classed "lighter"</p>
     <p className="small">A paragraph classed "small"</p>
-
   </>
-)
+);
