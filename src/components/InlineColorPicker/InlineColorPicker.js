5bad052fcdb7274d7e60ec0e66695191c27b579a
gffuma <fumagalli.gf@gmail.com>
2020-09-16 16:03:17 +0200

    Use react-color for color picker

---
+++ src/components/InlineColorPicker/InlineColorPicker.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import styles from './InlineColorPicker.module.scss'
+import { SketchPicker } from 'react-color'
+
+export default function InlineColorPicker({ color: maybeColor, onChange }) {
+  const [displayColorPicker, setDisplayColorPicker] = useState(false)
+  const color = maybeColor ?? '#000000' // Same as <input type='color' />
+
+  return (
+    <>
+      <div
+        className={styles.swatch}
+        onClick={() => setDisplayColorPicker(true)}
+      >
+        <div className={styles.color} style={{ background: color }} />
+      </div>
+      {displayColorPicker && (
+        <div className={styles.popover}>
+          <div
+            className={styles.cover}
+            onClick={() => setDisplayColorPicker(false)}
+          />
+          <SketchPicker
+            color={color}
+            onChangeComplete={(color) => onChange(color.hex)}
+          />
+        </div>
+      )}
+    </>
+  )
+}
