ea5c11387221e63ce4fcb3c6182eb140f5fde563
mikima <mikimauri+github@gmail.com>
2020-09-22 23:42:51 +0200

    updated charts

--- src/charts.js
+++ src/charts.js
@@ -1,31 +1,31 @@
 import {
-  scatterplot,
-  linechart,
-  matrixplot,
-  sunburst,
-  sankeydiagram,
-  alluvialdiagram,
-  beeswarm,
-  treemap,
-  circlepacking,
-  dendrogram,
-  colortest,
+	bubblechart,
+	linechart,
+	matrixplot,
+	sunburst,
+	sankeydiagram,
+	alluvialdiagram,
+	beeswarm,
+	treemap,
+	circlepacking,
+	dendrogram,
+	circularDendrogram,
 } from "@raw-temp/rawgraphs-charts";
 
 // New charts, not included into first release.
 // Comment at necessity.
 let charts = [
-  scatterplot,
-  matrixplot,
-  sunburst,
-    circlepacking,
-    treemap,
-    dendrogram,
-	  sankeydiagram,
-    alluvialdiagram,
-    beeswarm,
-  linechart,
-    colortest
+	bubblechart,
+	matrixplot,
+	sunburst,
+	circlepacking,
+	treemap,
+	dendrogram,
+	sankeydiagram,
+	alluvialdiagram,
+	beeswarm,
+	linechart,
+	circularDendrogram
 ]
 
 export default charts
