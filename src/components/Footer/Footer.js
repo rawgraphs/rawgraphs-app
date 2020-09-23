c62a666bf8531fa9b0f3a2387817a55abe8d2844
Mauro Bianchi <bianchimro@gmail.com>
2020-09-23 15:19:30 +0200

    showing hash in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,12 +5,14 @@ import { BsFillEnvelopeFill } from 'react-icons/bs'
 import { FaTwitter, FaGithub } from 'react-icons/fa'
 import { GiMoneyStack } from 'react-icons/gi'
 
+const commitHash = process.env.REACT_APP_VERSION || 'dev'
+
 export default function Footer(props) {
   return (
     <Container className={styles.footer} fluid>
         <Row>
           <Col xs={3}><p className="small">RAWGraphs is an open source project by DensityDesign Lab, Calibro and Inmagik.</p></Col>
-          <Col xs={3}><p className="small">This version is intended to be available only for the backers of the crowdfunding campaign.</p></Col>
+          <Col xs={3}><p className="small">This <span title={commitHash}>version</span> is intended to be available only for the backers of the crowdfunding campaign.</p></Col>
           <Col sm={{span:3, offset:0}} md={{span:3, offset:1}} xl={{span:2, offset:2}}>
             <p><BsFillEnvelopeFill/> hello [at] rawgraphs.io</p>
             <p><FaTwitter/> <a href="https://twitter.com/rawgraphs" target="_blank" rel="noopener noreferrer">@rawgraphs</a></p>
