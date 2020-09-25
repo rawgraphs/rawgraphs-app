a13a4b52f323029bd067bdaaf74348a29c8773ec
Mauro Bianchi <bianchimro@gmail.com>
2020-09-25 16:19:07 +0200

    fixed d-n-d: not allowing dragging columns into the same dimension drop zone

--- src/components/DataMapping/ChartDimensionCard.js
+++ src/components/DataMapping/ChartDimensionCard.js
@@ -57,7 +57,7 @@ const ChartDimensionCard = ({
             }
           : undefined,
       })
-      } else {
+      } else if(item.dimensionId !== dimension.id) {
         replaceDimension(
           item.dimensionId,
           dimension.id,
