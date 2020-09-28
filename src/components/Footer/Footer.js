5c0c616b0fb2c15cdb651496e17b1c6343257966
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-09-28 09:28:24 +0200

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,13 +12,13 @@ export default function Footer(props) {
     <Container fluid style={{backgroundColor: 'var(--dark)'}}>
       <Container className={styles.footer} >
         <Row>
-          <Col xs={6} sm={{span:6, order:1}} lg={{span:3,order:1}}><p className="Xsmall">RAWGraphs is an open source project desdigned and developed by <a href="http://densitydesign.org/" target="_blank" rel="noopener noreferrer">DensityDesign</a>, <a href="https://calib.ro/" target="_blank" rel="noopener noreferrer">Calibro</a> and <a href="https://inmagik.com/" target="_blank" rel="noopener noreferrer">Inmagik</a>.</p></Col>
-          <Col xs={6} sm={{span:6, order:3}} lg={{span:3,order:1}}><p className="Xsmall">This <span title={commitHash}>version</span> is intended to be available only for the backers of the crowdfunding campaign.</p></Col>
-          <Col sm={{span:6, order:2}} md={{span:3}} xl={{span:2, offset:2}}>
-            <p><BsFillEnvelopeFill/> hello <span className="small">AT</span> rawgraphs.io</p>
+          <Col xs={6} sm={{span:5, order:1}} lg={{span:3,order:1}}><p className="Xsmall">RAWGraphs is an open source project desdigned and developed by <a href="http://densitydesign.org/" target="_blank" rel="noopener noreferrer">DensityDesign</a>, <a href="https://calib.ro/" target="_blank" rel="noopener noreferrer">Calibro</a> and <a href="https://inmagik.com/" target="_blank" rel="noopener noreferrer">Inmagik</a>.</p></Col>
+          <Col xs={6} sm={{span:5, order:3}} lg={{span:3,order:1}}><p className="Xsmall">This <span title={commitHash}>version</span> is intended to be available only for the backers of the crowdfunding campaign.</p></Col>
+          <Col xs={6} sm={{span:6, offset:1, order:2}} md={{span:3}} lg={{offset:0}} xl={{span:2, offset:2}}>
+            <p><BsFillEnvelopeFill/> hello at rawgraphs.io</p>
             <p><FaTwitter/> <a href="https://twitter.com/rawgraphs" target="_blank" rel="noopener noreferrer">@rawgraphs</a></p>
           </Col>
-          <Col sm={{span:6, order:4}} md={{span:2}} xl={{span:2, offset:0}}>
+          <Col xs={6} sm={{span:6, offset:1, order:4}} md={{span:2}} lg={{offset:0}} xl={{span:2, offset:0}}>
             <p><FaGithub/> <a href="https://github.com/rawgraphs" target="_blank" rel="noopener noreferrer">Github</a></p>
             <p><GiMoneyStack/> <a href="https://www.indiegogo.com/projects/rawgraphs-2-0-a-web-app-for-data-visualization#/" target="_blank" rel="noopener noreferrer">Support</a></p>
           </Col>
