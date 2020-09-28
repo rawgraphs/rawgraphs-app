7742a624659a310565a8c809bf8d17e80bd2d1af
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-09-28 09:28:22 +0200

    Update ScreenSizeAlert.js

--- src/components/ScreenSizeAlert/ScreenSizeAlert.js
+++ src/components/ScreenSizeAlert/ScreenSizeAlert.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import useWindowSize from '../../hooks/useWindowSize'
 import { Modal, Button } from 'react-bootstrap'
 import { BsEnvelopeFill } from 'react-icons/bs'
 
@@ -6,21 +7,26 @@ import { BsEnvelopeFill } from 'react-icons/bs'
 
 function ScreenSizeAlert() {
 	const size = useWindowSize();
-	
 	const [showModal, setShowModal] = useState(size.width<992);
+	const [modalWasClosed, setModalWasClosed] = useState(false);
 
-	const handleClose = () => setShowModal(false);
-	// const handleShow = () => setShowModal(true);
+	const handleClose = () => {
+		setShowModal(false);
+		setModalWasClosed(true);
+	};
+	const handleShow = () => setShowModal(true);
 
 	useEffect(() => {
-		setShowModal(size.width<992);
+		if (modalWasClosed===false) {
+			setShowModal(size.width<992);
+		}
   }, [size]);
 
 	return (
 			<Modal
 				className="raw-modal"
 				show={showModal}
-				onHide={()=>setShowModal(false)}
+				onHide={handleClose}
 				backdrop="static"
 				keyboard={false}
 				// size="lg"
@@ -28,50 +34,18 @@ function ScreenSizeAlert() {
 				centered
 			>
 				<Modal.Header closeButton>
-					<Modal.Title as="h5">🎉 Welcome to the new RAWGraphs!</Modal.Title>
+					<Modal.Title as="h5"><span role="img" aria-label="Party icon">🎉</span> Welcome to the new RAWGraphs!</Modal.Title>
 				</Modal.Header>
 				<Modal.Body>
-					<p className="big">RAWGraphs 2.0 is designed for {size.width >= 768?'slightly ':' '}bigger desktop screens!</p>
-					<p>Make your browser window bigger or to <a href="mailto:?subject=Visit+RAWGraphs+2.0&body=Hello%21%0D%0APlease+do+not+forget+to+take+a+look+at+the+new+version+of+RAWGraphs%21%0D%0A%0D%0AVisit%3A+https%3A%2F%2Fdev.rawgraphs.io%2F%0D%0A%0D%0ASee+you+later%2C%0D%0AThe+RAWGraphs+Team"><BsEnvelopeFill/> send yourself a reminder</a> to come back at a better time.</p>
+					<p className="big">RAWGraphs 2.0 is designed for {size.width >= 768?'slightly ':' '}bigger screens!</p>
+					<p>Resize your browser window or <a href="mailto:?subject=Visit+RAWGraphs+2.0&body=Hello%21%0D%0APlease+do+not+forget+to+take+a+look+at+the+new+version+of+RAWGraphs%21%0D%0A%0D%0AVisit%3A+https%3A%2F%2Fdev.rawgraphs.io%2F%0D%0A%0D%0ASee+you+later%2C%0D%0AThe+RAWGraphs+Team"><BsEnvelopeFill/> send yourself a reminder</a> to come back at a better time.</p>
 					<p>Touch devices are not fully supported yet.</p>
 				</Modal.Body>
 				<Modal.Footer>
-					<Button variant="primary">Got it!</Button>
+					<Button variant="primary" onClick={handleClose}>Got it!</Button>
 				</Modal.Footer>
 			</Modal>
 	)
 }
 
 export default ScreenSizeAlert
-
-// Hook
-function useWindowSize() {
-  // Initialize state with undefined width/height so server and client renders match
-  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
-  });
-
-  useEffect(() => {
-    // Handler to call on window resize
-    function handleResize() {
-      // Set window width/height to state
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    }
-    
-    // Add event listener
-    window.addEventListener("resize", handleResize);
-    
-    // Call handler right away so state gets updated with initial window size
-    handleResize();
-    
-    // Remove event listener on cleanup
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount
-
-  return windowSize;
-}
