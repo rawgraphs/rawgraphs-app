d3973d5237becb565724c54b11335a850231f9ee
mikima <mikimauri+github@gmail.com>
2020-10-06 15:26:18 +0200

    added circular dendrogram

--- src/charts.js
+++ src/charts.js
@@ -1,31 +1,31 @@
 import {
-	bubblechart,
-	linechart,
-	matrixplot,
-	sunburst,
-	// sankeydiagram,
-	// alluvialdiagram,
-	// beeswarm,
-	// treemap,
-	// circlepacking,
-	// dendrogram,
-	// circularDendrogram,
-} from "@raw-temp/rawgraphs-charts";
+  bubblechart,
+  linechart,
+  matrixplot,
+  sunburst,
+  // sankeydiagram,
+  // alluvialdiagram,
+  // beeswarm,
+  // treemap,
+  // circlepacking,
+  // dendrogram,
+  circularDendrogram,
+} from '@raw-temp/rawgraphs-charts'
 
 // New charts, not included into first release.
 // Comment at necessity.
 let charts = [
   bubblechart,
   linechart,
-	matrixplot,
+  matrixplot,
   sunburst,
   // sankeydiagram,
   // alluvialdiagram,
   // beeswarm,
-	// circlepacking,
-	// treemap,
-	// dendrogram,
-	// circularDendrogram
+  // circlepacking,
+  // treemap,
+  // dendrogram,
+  circularDendrogram,
 ]
 
 export default charts
