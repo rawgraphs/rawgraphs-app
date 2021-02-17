59b2df7627db3f52b42199fef8f25a2a0d1eef85
uf0 <scnepa222@gmail.com>
2021-02-17 17:03:25 +0100

    close #58

--- src/components/DataSamples/DataSamples.js
+++ src/components/DataSamples/DataSamples.js
@@ -50,6 +50,42 @@ const samplesList = [
     url: './sample-datasets/CirclePacking - biggest-cities.tsv',
     delimiter: '\t',
   },
+
+  {
+    name: 'FIFA playes statistics',
+    category: 'Radar Chart',
+    url: './sample-datasets/Radar Chart - Fifa players.tsv',
+    delimiter: '\t',
+  },
+
+  {
+    name: 'Premier League and Serie A ranking',
+    category: 'Slop Graph',
+    url: './sample-datasets/Slope graph - Premier SerieA ranking.tsv',
+    delimiter: '\t',
+  },
+
+  {
+    name: 'Olympics Medals',
+    category: 'Streamgraph',
+    url: './sample-datasets/Streamgraph - Olympics Medals.tsv',
+    delimiter: '\t',
+  },
+
+  {
+    name: 'Italian Presidents',
+    category: 'Gantt chart',
+    url: './sample-datasets/Gantt chart - Italian Presidents.tsv',
+    delimiter: '\t',
+  },
+
+  {
+    name: 'Basketball shots',
+    category: 'Hexagonal Binning',
+    url: './sample-datasets/Hexbin - basketball shots.tsv',
+    delimiter: '\t',
+  },
+
 ]
 export default function DataSamples({ onSampleReady, setLoadingError }) {
   const select = async (sample) => {
