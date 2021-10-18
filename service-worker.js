445abe8704352993b4871e2faf76119355beef66
mikima <mikima@users.noreply.github.com>
2021-10-18 12:24:41 +0000

    deploy: 7d3ad956cf19ffb297f8c3670fd7619c7418ec57

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.282b2bce2c5c13e7029cdaf9fbad8ee1.js"
+  "/precache-manifest.0682a302d092d58185f4ff4de828ed8c.js"
 );
 
 self.addEventListener('message', (event) => {
