3c5be44820a425816fd0c34ea368ddc1fdd3df8a
bianchimro <bianchimro@gmail.com>
2023-11-08 12:06:43 +0100

    updated header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import { Navbar, Nav } from 'react-bootstrap'
 export default function Header({ menuItems }) {
   return (
     <Navbar bg="white" expand="lg" sticky="top" className={styles.navbar}>
-      <Navbar.Brand href="/"><b>RAW</b><span className="text-primary">Graphs</span> 2.0 beta</Navbar.Brand>
+      <Navbar.Brand href="/"><b>RAW</b><span className="text-primary">Graphs</span> 2.0</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
@@ -24,6 +24,15 @@ export default function Header({ menuItems }) {
             className="btn btn-sm btn-primary ml-2 d-flex flex-column align-items-center justify-content-center"
           >
             Report issue
+          </a>
+          <a
+            role="button"
+            href="https://github.com/sponsors/rawgraphs"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-sm btn-primary ml-2 d-flex flex-column align-items-center justify-content-center"
+          >
+            Support us
           </a>
         </Nav>
       </Navbar.Collapse>
